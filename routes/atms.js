b0735697a56088f308d03e9b8c2d4781f96e4a6c
Manuel Porto <manu.porto94@gmail.com>
2019-08-29 12:29:13 -0300

    Add filters for ATM's bank and network

--- routes/atms.js
+++ routes/atms.js
@@ -1,17 +1,23 @@
-import { Router } from 'express';
-import csv from 'csvtojson';
-
+import { Router } from "express";
+import csv from "csvtojson";
 
 const router = Router();
-const csvFilePath = 'cajeros-automaticos.csv';
-
+const csvFilePath = "cajeros-automaticos.csv";
 
 /* GET atms listing. */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res) => {
+  const { bank, network } = req.query;
   csv()
     .fromFile(csvFilePath)
-    .then((jsonObj) => {
-      res.send(jsonObj[0]);
+    .then(jsonObj => {
+      const filteredAtms = jsonObj.filter(atm => {
+        const bankFilter =
+          !bank || (bank && atm.banco.localeCompare(bank) === 0);
+        const networkFilter =
+          !network || (network && atm.red.localeCompare(network) === 0);
+        return bankFilter && networkFilter;
+      });
+      res.send(filteredAtms[0]);
     });
 });
 
