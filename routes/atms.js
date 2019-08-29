c1a85de1a9c5b6098bc2cf77512972d2757593e8
Manuel Porto <manu.porto94@gmail.com>
2019-08-29 19:02:23 -0300

    Add filtering by distance

--- routes/atms.js
+++ routes/atms.js
@@ -4,20 +4,47 @@ import csv from "csvtojson";
 const router = Router();
 const csvFilePath = "cajeros-automaticos.csv";
 
+const deg2rad = deg => deg * (Math.PI / 180);
+const calculateDistance = (origin, destination) => {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(destination.lat - origin.lat);
+  const dLon = deg2rad(destination.lon - origin.lon);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(origin.lat)) *
+      Math.cos(deg2rad(destination.lat)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+};
+
 /* GET atms listing. */
 router.get("/", (req, res) => {
-  const { bank, network } = req.query;
+  const { bank, distance, network } = req.query;
+  const origin = {
+    lat: Number(req.query.originLat),
+    lon: Number(req.query.originLon)
+  };
   csv()
     .fromFile(csvFilePath)
     .then(jsonObj => {
       const filteredAtms = jsonObj.filter(atm => {
+        const destination = {
+          lat: Number(atm.lat),
+          lon: Number(atm.long)
+        };
         const bankFilter =
           !bank || (bank && atm.banco.localeCompare(bank) === 0);
         const networkFilter =
           !network || (network && atm.red.localeCompare(network) === 0);
-        return bankFilter && networkFilter;
+        const distanceFilter =
+          (!distance && calculateDistance(origin, destination) < 0.5) ||
+          (distance && calculateDistance(origin, destination) < distance);
+        return bankFilter && networkFilter && distanceFilter;
       });
-      res.send(filteredAtms[0]);
+      res.send(filteredAtms);
     });
 });
 
